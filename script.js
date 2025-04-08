a0a8f7665a6ca594c91a8c0039df795a1e8c8e0f
YusufXojimurodov <xojimurodovyusufjon2@gmail.com>
2025-04-08 23:16:14 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function tekshirish() {
 
   if (login == "Abdurasul" && parol == "2009") {
     window.location.href = "lattecafe.html"
-  } else {
-    document.getElementById("error").innerText = "Login yoki Parol xato ! Saytga kirishga ruxsat yo'q"
-  }
-
-  if ( login == "Otabek" && parol == "4554") {
+  } else if (login == "Yusuf" && parol == "2010") {
+    window.location.href = "lattecafe.html"
+  } else if (login == "Emir" && parol == "2394") {
+    window.location.href = "lattecafe.html"
+  } else if (login == "Otabek" && parol == "4554") {
     window.location.href = "lattecafe.html"
   } else {
     document.getElementById("error").innerText = "Login yoki Parol xato ! Saytga kirishga ruxsat yo'q"
